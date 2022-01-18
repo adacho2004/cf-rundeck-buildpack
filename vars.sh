8386690189e167cdcaa74162f1d7a26624fa3da5
adacho2004 <61000022+adacho2004@users.noreply.github.com>
2022-01-18 18:55:56 +0900

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -2,6 +2,7 @@
 
 export JRE_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz
 #export RUNDECK_DIST_URL="https://repo1.maven.org/maven2/org/rundeck/rundeck/${RUNDECK_VERSION}/"
-RUNDECK_DIST_URL="https://download.rundeck.org/war/"
+export RUNDECK_WAR="rundeckpro-enterprise-${RUNDECK_VERSION}.war"
+export RUNDECK_DIST_URL="https://packagecloud.io/pagerduty/rundeckpro/packages/java/com.rundeck.enterprise/${RUNDECK_WAR}/artifacts/${RUNDECK_WAR}/download"
 #export RUNDECK_WAR="rundeck-${RUNDECK_VERSION}.war"
-RUNDECK_WAR="rundeckpro-enterprise-${RUNDECK_VERSION}.war"
+
